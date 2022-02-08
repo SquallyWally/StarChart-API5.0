58b7cded09f503726b61181b84d1cfb2ba2e77ea
milo_k <milo_kastablank@outlook.com>
2022-02-08 14:01:39 +0100

    Added Tests Added EF Configured Middleware

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StarChart.Data;
 
 namespace StarChart
 {
@@ -27,6 +22,8 @@ namespace StarChart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "StarChart", Version = "v1"}); });
         }
 
@@ -43,6 +40,8 @@ namespace StarChart
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseMvc();
 
             app.UseAuthorization();
 
